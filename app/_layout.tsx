8039eb3d115add5e9d12afc678e8254daaba5dc0
jonspenser <jonspenser@gmail.com>
2026-05-20 22:37:22 -1000

    Diagnostic: bare hello world to test SDK 54/iOS 26 baseline

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,23 +1,9 @@
 import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { SwellLogProvider } from '../contexts/SwellLogContext';
 
 export default function RootLayout() {
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <SwellLogProvider>
-        <StatusBar style="light" backgroundColor="#000000" />
-        <Stack screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="index" />
-          <Stack.Screen name="buoy/[id]" options={{ gestureEnabled: true, gestureDirection: 'horizontal' }} />
-          <Stack.Screen name="buoys" options={{ gestureEnabled: true, gestureDirection: 'horizontal' }} />
-          <Stack.Screen name="forecast" options={{ gestureEnabled: true, gestureDirection: 'horizontal' }} />
-          <Stack.Screen name="logbook" options={{ gestureEnabled: false }} />
-          <Stack.Screen name="alerts" options={{ gestureEnabled: true, gestureDirection: 'horizontal' }} />
-          <Stack.Screen name="micwind" options={{ gestureEnabled: true, gestureDirection: 'horizontal' }} />
-        </Stack>
-      </SwellLogProvider>
-    </GestureHandlerRootView>
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="index" />
+    </Stack>
   );
 }
